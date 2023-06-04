e981c0a634e33af81d2145048fd829e7cb6d8500
Ekrem <ekrem@botekrem.dev>
2023-06-05 00:01:16 +0300

    Frontend Fixes

---
+++ components/images/ImageWithBlur.tsx
@@ -0,0 +1,26 @@
+import Image, { ImageProps } from 'next/image';
+import React from 'react';
+
+
+interface ImageWithFallbackProps extends ImageProps {
+  alt: string;
+  fallbackUrl: string;
+  src: string;
+  fill?: boolean;
+}
+
+export const ImageWithBlur = ({alt, fallbackUrl, fill = false, ...props}: ImageWithFallbackProps) => {
+  const [src, setSrc] = React.useState(props.src);
+
+  return (
+    <Image
+      {...props}
+      src={src+ "?" + Date.now()}
+      alt={alt}
+      fill={fill}
+      placeholder={"blur"}
+      blurDataURL={fallbackUrl}
+      onError={(e) => { setSrc(fallbackUrl) }}
+    />
+  );
+};
