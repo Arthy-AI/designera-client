847e2d49a4f5506dd56d3578f3b672a9072c18d5
Ömer Şanlı <omer@arthylabs.com>
2023-11-30 18:22:19 +0300

    Publish Button Fix

--- components/images/gallery/GeneratedImageModalFooter.tsx
+++ components/images/gallery/GeneratedImageModalFooter.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowRotateLeft,
   faArrowUpRightFromSquare, faCopy,
@@ -7,23 +7,23 @@ import {
   faHeart,
   faPaperPlane, faWandMagicSparkles
 } from "@fortawesome/free-solid-svg-icons";
-import {IconButton} from "../../button/IconButton";
-import {AnimatedSimpleInput} from "../../input/AnimatedSimpleInput";
-import {DynamicObject} from "../../../constants/DynamicObject";
-import {imagesGlobal, imagesGlobalStore} from "../../../globals/images/images";
+import { IconButton } from "../../button/IconButton";
+import { AnimatedSimpleInput } from "../../input/AnimatedSimpleInput";
+import { DynamicObject } from "../../../constants/DynamicObject";
+import { imagesGlobal, imagesGlobalStore } from "../../../globals/images/images";
 import toast from "react-hot-toast";
-import {useAxios} from "../../../hooks/useAxios";
-import {ForceDownload} from "../../../constants/ForceDownload";
+import { useAxios } from "../../../hooks/useAxios";
+import { ForceDownload } from "../../../constants/ForceDownload";
 import useAsTheme from "../../../hooks/themes/useAsTheme";
 import useAuth from "../../../hooks/auth/useAuth";
 import useSubscription from "../../../hooks/subscription/useSubscription";
-import {Tooltip as ReactTooltip} from "react-tooltip";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
-export const GeneratedImageModalFooter = ({innerProps, isModal, currentIndex}: DynamicObject, closeLB: () => void) => {
-  const {userData, upvoteImageUpdate} = useAuth()
-  const {themesSectionToggle, themes, removeImageById, addImage} = useAsTheme()
-  const {POST, PATCH} = useAxios()
-  const {toggleModal: subscriptionToggleModal} = useSubscription()
+export const GeneratedImageModalFooter = ({ innerProps, isModal, currentIndex }: DynamicObject, closeLB: () => void) => {
+  const { userData, upvoteImageUpdate } = useAuth()
+  const { themesSectionToggle, themes, removeImageById, addImage } = useAsTheme()
+  const { POST, PATCH } = useAxios()
+  const { toggleModal: subscriptionToggleModal } = useSubscription()
   const [photos, setPhotos] = useState([] as any[])
   const [publishDescription, setPublishDescription] = useState("")
   const [publisheds, setPublisheds] = useState([] as String[]);
@@ -90,50 +90,50 @@ export const GeneratedImageModalFooter = ({innerProps, isModal, currentIndex}: D
   return isModal ? (
     <div {...innerProps}>
       {!publisheds?.includes(photos[currentIndex]?.id) &&
-          <div className={"w-full flex justify-center"}>
-              <div className={"w-full flex flex-row absolute bottom-5 justify-center"}>
-                  <AnimatedSimpleInput
-                      labelText={"Give your design a name"}
-                      className={"w-2/12 xl:w-3/12 alwaysOnTop"}
-                      labelClassname={"ml-5 text-white text-base"}
-                      inputClassname={"bg-[#1E1E1E] hover:bg-[#242424] focus:bg-[#242424] opacity-90 pl-7 pt-6"}
-                      value={publishDescription}
-                      onValueChange={(text) => {
-                        setPublishDescription(text)
-                      }}
-                  />
-                  <ReactTooltip
-                      anchorId={"publishButton"}
-                      place="top"
-                      content={"Publish"}
-                      delayShow={500}
-                  />
-                  <button
-                      id={"publishButton"}
-                      onClick={() => {
-                        if ((userData?.subscription?.isActive)) {
-                          publish()
-                        } else {
-                          closeLB()
-                          subscriptionToggleModal(true)
-                        }
-                      }}
-                      disabled={publishDescription.length < 6 || publishDescription.length > 50}
-                      className="xl:w-1/12 alwaysOnTop block text-stone-400 p-2 bg-[#1E1E1E] hover:bg-[#242424] focus:bg-[#242424] opacity-90 border border-[#6F6B6A] font-semibold hover:text-white designera-rounded ml-2 flex items-center justify-center"
-                      style={{height: 56, width: 56}}
-                  >
-                      <FontAwesomeIcon icon={faPaperPlane} color={"#AAA7A5"}
-                                       style={{paddingRight: 2, height: 30, width: 30}}/>
-                  </button>
-              </div>
+        <div className={"w-full flex justify-center"}>
+          <div className={"w-full flex flex-row absolute bottom-5 justify-center"}>
+            <AnimatedSimpleInput
+              labelText={"Give your design a name"}
+              className={"w-2/12 xl:w-3/12 alwaysOnTop"}
+              labelClassname={"ml-5 text-white text-base"}
+              inputClassname={"bg-[#1E1E1E] hover:bg-[#242424] focus:bg-[#242424] opacity-90 pl-7 pt-6"}
+              value={publishDescription}
+              onValueChange={(text) => {
+                setPublishDescription(text)
+              }}
+            />
+            <ReactTooltip
+              anchorId={"publishButton"}
+              place="top"
+              content={"Publish"}
+              delayShow={500}
+            />
+            <button
+              id={"publishButton"}
+              onClick={() => {
+                if (publishDescription.length >= 6 && publishDescription.length <= 50) {
+                  publish();
+                } else {
+                  closeLB();
+                  subscriptionToggleModal(true);
+                }
+              }}
+              disabled={publishDescription.length < 6 || publishDescription.length > 50}
+              className="xl:w-1/12 alwaysOnTop block text-stone-400 p-2 bg-[#1E1E1E] hover:bg-[#242424] focus:bg-[#242424] opacity-90 border border-[#6F6B6A] font-semibold hover:text-white designera-rounded ml-2 flex items-center justify-center"
+              style={{ height: 56, width: 56 }}
+            >
+              <FontAwesomeIcon icon={faPaperPlane} color={"#AAA7A5"}
+                style={{ paddingRight: 2, height: 30, width: 30 }} />
+            </button>
           </div>
+        </div>
       }
       <div className={"px-2 w-fit absolute left-0 bottom-5 flex flex-row gap-4"}>
         <IconButton
           description={"Download"}
           icon={
             <FontAwesomeIcon icon={faCircleDown} color={"#AAA7A5"} size={"xl"}
-                             style={{width: 25, height: 25}}/>
+              style={{ width: 25, height: 25 }} />
           }
           onClick={() => ForceDownload(photos[currentIndex].src, "designera-" + photos[currentIndex].id)}
         />
@@ -142,7 +142,7 @@ export const GeneratedImageModalFooter = ({innerProps, isModal, currentIndex}: D
           icon={
             <FontAwesomeIcon icon={faHeart} color={
               userData?.upvotedImages?.findIndex((v: any) => v.id == photos[currentIndex].id) == -1 ? "#AAA7A5" : "#FF6363"
-            } size={"xl"} style={{width: 25, height: 25}}/>
+            } size={"xl"} style={{ width: 25, height: 25 }} />
           }
           onClick={() => vote(true)}
         />
@@ -151,18 +151,18 @@ export const GeneratedImageModalFooter = ({innerProps, isModal, currentIndex}: D
           icon={<FontAwesomeIcon icon={faWandMagicSparkles} color={
             themes.findIndex((v) => v.id == photos[currentIndex].id) == -1 ? "#AAA7A5" : "#61A0FF"
           } size={"xl"}
-                                 style={{width: 25, height: 25}}/>} onClick={() => {
-          if (themes.findIndex((v) => v.id == photos[currentIndex].id) == -1) {
-            addImage({
-              id: photos[currentIndex].id,
-              url: photos[currentIndex].id,
-              style: photos[currentIndex].style
-            })
-          } else {
-            removeImageById(photos[currentIndex].id)
-          }
-          themesSectionToggle(true)
-        }}/>
+            style={{ width: 25, height: 25 }} />} onClick={() => {
+              if (themes.findIndex((v) => v.id == photos[currentIndex].id) == -1) {
+                addImage({
+                  id: photos[currentIndex].id,
+                  url: photos[currentIndex].id,
+                  style: photos[currentIndex].style
+                })
+              } else {
+                removeImageById(photos[currentIndex].id)
+              }
+              themesSectionToggle(true)
+            }} />
       </div>
     </div>
   ) : null;
