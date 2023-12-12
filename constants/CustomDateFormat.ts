635f039d185cbf7f6c7b624d9b6a0c1d4366f084
OmerSanli <omer@arthylabs.com>
2023-12-12 23:12:48 +0700

    CustomDate Formats Minor Fixes

--- constants/CustomDateFormat.ts
+++ constants/CustomDateFormat.ts
@@ -4,24 +4,34 @@
 
 import moment from 'moment';
 
-export function CustomDateFormat(this: any) {
+export function CustomDateFormat(this: { duration: number }) {
   const duration = moment.duration(this.duration);
 
+  const formatTimeUnit = (value: number, singular: string, plural: string): string => {
+    return value === 1 ? `1 [${singular}]` : `${value} [${plural}]`;
+  };
+
   if (duration.asSeconds() < 60) {
-    return "s [Seconds]";
-  } else if (duration.asMinutes() < 10) {
-    return "m [Minutes], s [Seconds]";
+    return formatTimeUnit(duration.seconds(), 'Second', 'Seconds');
   } else if (duration.asMinutes() < 60) {
-    return "m [Minutes]";
-  } else if (duration.asHours() < 6) {
-    return "H [Hours], m [Minutes]";
+    let formatted = formatTimeUnit(duration.minutes(), 'Minute', 'Minutes');
+    if (duration.asMinutes() >= 10) {
+      formatted += `, ${formatTimeUnit(duration.seconds(), 'Second', 'Seconds')}`;
+    }
+    return formatted;
   } else if (duration.asHours() < 24) {
-    return "H [Hours]";
-  } else if (duration.asDays() < 7) {
-    return "d [Days]";
+    let formatted = formatTimeUnit(duration.hours(), 'Hour', 'Hours');
+    if (duration.asHours() < 6) {
+      formatted += `, ${formatTimeUnit(duration.minutes(), 'Minute', 'Minutes')}`;
+    }
+    return formatted;
   } else if (duration.asDays() < 30) {
-    return "w [Weeks]";
+    let formatted = formatTimeUnit(duration.days(), 'Day', 'Days');
+    if (duration.asDays() < 7) {
+      formatted += `, ${formatTimeUnit(duration.hours(), 'Hour', 'Hours')}`;
+    }
+    return formatted;
   } else {
-    return "M [Months]";
+    return formatTimeUnit(duration.months(), 'Month', 'Months');
   }
 }
